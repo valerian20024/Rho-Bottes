a1a0003c5dcde9f285ee09bdd417811214cc0d4d
Wislez Valerian <valerian@valdev.leomigaland.be>
2025-02-01 18:16:28 +0100

    Added the files

---
+++ src/plugins/greeting.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import random as rand
+import numpy as np
+
+class GreetingCog(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command()
+    async def hello(self, ctx: commands.Context) -> discord.Message:
+        
+        return await ctx.send("Hello")
+    
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(GreetingCog(bot))
